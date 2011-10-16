3d14c470440c62eba8cbd89cc1febf58021e8163
Andrew Chilton <chilts@appsattic.com>
2011-10-17 11:06:57 +1300

    Skeleton to show what env vars to fill out for the test scripts

---
+++ examples/set-env.sh
@@ -0,0 +1,9 @@
+# Please set the following ENV VARS for the specific tests:
+
+# Amazon : SimpleDB
+export ACCESS_KEY_ID=access-key-id
+export SECRET_ACCESS_KEY=secret-access-key
+
+# RackspaceCloud : ToDo!
+
+# etc...
