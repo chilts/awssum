d8c34ab60f8d3e31356c1560f8159364fc01bbc8
Andrew Chilton <chilts@appsattic.com>
2011-10-17 10:41:47 +1300

    Add SimpleDB:DeleteDomain and factor out performRequest()

---
+++ examples/test-simplejs-delete-domain.js
@@ -0,0 +1,14 @@
+var amazon = require("../lib/amazon");
+var simpledb = require("../lib/simpledb");
+
+var sdb = new simpledb.SimpleDB('key', 'secret', amazon.US_WEST_1);
+
+console.log( 'Region :', sdb.region() );
+console.log( 'EndPoint :',  sdb.endPoint() );
+console.log( 'AccessKeyId :', sdb.accessKeyId() );
+console.log( 'SecretAccessKey :', sdb.secretAccessKey() );
+
+sdb.deleteDomain('test', function(err, data) {
+    console.log('Error :', err);
+    console.log('Data  :', data);
+});
