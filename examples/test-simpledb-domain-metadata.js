d003d1add16f361daac0e37133fe191fb489920f
Andrew Chilton <chilts@appsattic.com>
2011-10-17 22:28:45 +1300

    Add SimpleDB:DomainMetadata operation

---
+++ examples/test-simpledb-domain-metadata.js
@@ -0,0 +1,18 @@
+var amazon = require("../lib/amazon");
+var simpledb = require("../lib/simpledb");
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+var sdb = new simpledb.SimpleDB(accessKeyId, secretAccessKey, amazon.US_WEST_1);
+
+console.log( 'Region :', sdb.region() );
+console.log( 'EndPoint :',  sdb.endPoint() );
+console.log( 'AccessKeyId :', sdb.accessKeyId() );
+console.log( 'SecretAccessKey :', sdb.secretAccessKey() );
+
+sdb.domainMetadata('test', function(err, data) {
+    console.log('Error :', err);
+    console.log('Data  :', data);
+});
