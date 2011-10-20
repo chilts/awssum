54d7dd4d184fc1699365e485d3355dcef49f41ed
Andrew Chilton <chilts@appsattic.com>
2011-10-21 07:42:47 +1300

    Add the SQS:SendMessage operation

---
+++ examples/amazon-sqs-send-message.js
@@ -0,0 +1,27 @@
+var amazon = require("../lib/amazon");
+var sqs = require("../lib/sqs");
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var sqs = new sqs.Sqs(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', sqs.region() );
+console.log( 'EndPoint :',  sqs.endPoint() );
+console.log( 'AccessKeyId :', sqs.accessKeyId() );
+console.log( 'SecretAccessKey :', sqs.secretAccessKey() );
+console.log( 'AwsAccountId :', sqs.awsAccountId() );
+
+sqs.sendMessage('my-queue', 'Hello, World!', function(err, data) {
+    console.log("\nSending a message to a queue - expecting success");
+    console.log('Error :', err);
+    console.log('Data  :', data);
+});
+
+sqs.sendMessage('new-queue', undefined, function(err, data) {
+    console.log("\nSending an undefined message - expecting failure");
+    console.log('Error :', err);
+    console.log('Data  :', data);
+});
