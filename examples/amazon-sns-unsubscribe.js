f091ce23fc68d5edcd2720c3e110796a96cc60e6
Andrew Chilton <chilts@appsattic.com>
2011-10-21 17:49:14 +1300

    Add the SNS:Unsubscribe operation

---
+++ examples/amazon-sns-unsubscribe.js
@@ -0,0 +1,28 @@
+var util = require('util');
+var amazon = require('../lib/amazon');
+var snsService = require('../lib/sns');
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var sns = new snsService.Sns(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', sns.region() );
+console.log( 'EndPoint :',  sns.endPoint() );
+console.log( 'AccessKeyId :', sns.accessKeyId() );
+console.log( 'SecretAccessKey :', sns.secretAccessKey() );
+console.log( 'AwsAccountId :', sns.awsAccountId() );
+
+sns.unsubscribe('fakeSubscriptionArn', function(err, data) {
+    console.log("\nUnsubscribing this subscriptionArn - expecting failure");
+    console.log('Error :', err);
+    console.log('Data  :', data);
+});
+
+sns.unsubscribe(undefined, function(err, data) {
+    console.log("\nUnsubscribing an undefined subscriptionArn - expecting failure");
+    console.log('Error :', err);
+    console.log('Data  :', data);
+});
