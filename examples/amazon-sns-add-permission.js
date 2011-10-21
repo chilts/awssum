074cb294bdee1dcb0544c42651696894fab088b1
Andrew Chilton <chilts@appsattic.com>
2011-10-22 10:23:51 +1300

    Add the SNS:AddPermission operation

---
+++ examples/amazon-sns-add-permission.js
@@ -0,0 +1,34 @@
+var util = require('util');
+var amazon = require('../lib/amazon');
+var snsService = require('../lib/sns');
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var sns = new snsService.Sns(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', sns.region() );
+console.log( 'EndPoint :',  sns.endPoint() );
+console.log( 'AccessKeyId :', sns.accessKeyId() );
+console.log( 'SecretAccessKey :', sns.secretAccessKey() );
+console.log( 'AwsAccountId :', sns.awsAccountId() );
+
+// firstly, re-create this topic (it's idempotent) to get the topicArn
+sns.createTopic('my-topic', function(err, data) {
+    console.log("\nCreating (my-topic) - expecting success");
+    console.log('Error :', err);
+    console.log('Data  :', data);
+
+    // now call the addPermission()
+    if ( ! err ) {
+        var topicArn = data.CreateTopicResponse.CreateTopicResult.TopicArn;
+        var newPermission = { awsAccountId : '123-456-789', actionName : 'NoExistantAction' };
+        sns.addPermission(topicArn, 'A Test Permission', [ newPermission ], function(err, data) {
+            console.log("\naddPermission() - expecting failure (for many reasons)");
+            console.log('Error :', util.inspect(err, true, null));
+            console.log('Data :', util.inspect(data, true, null));
+        });
+    }
+});
