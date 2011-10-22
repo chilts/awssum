9aa81510571fc6b817ba829dd9c2cdbca35858cd
Andrew Chilton <chilts@appsattic.com>
2011-10-22 21:53:09 +1300

    Add the SQS:SendMessageBatch operation

---
+++ examples/amazon-sqs-send-message-batch.js
@@ -0,0 +1,28 @@
+var util = require('util');
+var amazon = require("../lib/amazon");
+var sqs = require("../lib/sqs");
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var sqs = new sqs.Sqs(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', sqs.region() );
+console.log( 'EndPoint :',  sqs.endPoint() );
+console.log( 'AccessKeyId :', sqs.accessKeyId() );
+console.log( 'SecretAccessKey :', sqs.secretAccessKey() );
+console.log( 'AwsAccountId :', sqs.awsAccountId() );
+
+var messages = [
+    { id : 'janelle', messageBody : 'janelle' },
+    { id : 'lucy',    messageBody : 'lucy'    },
+    { id : 'sarah',   messageBody : 'sarah', delaySeconds : 20 }
+];
+
+sqs.sendMessageBatch('my-queue', messages, function(err, data) {
+    console.log("\nSending a message batch to a queue - expecting success");
+    console.log('Error :', util.inspect(err, true, null));
+    console.log('Data :', util.inspect(data, true, null));
+});
