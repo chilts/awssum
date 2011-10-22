3ba94765d614988e29b74abb3f3cdc580dda7546
Andrew Chilton <chilts@appsattic.com>
2011-10-22 20:48:18 +1300

    Add the SQS:GetQueueUrl operation

---
+++ examples/amazon-sqs-get-queue-url.js
@@ -0,0 +1,22 @@
+var util = require('util');
+var amazon = require("../lib/amazon");
+var sqs = require("../lib/sqs");
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var sqs = new sqs.Sqs(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', sqs.region() );
+console.log( 'EndPoint :',  sqs.endPoint() );
+console.log( 'AccessKeyId :', sqs.accessKeyId() );
+console.log( 'SecretAccessKey :', sqs.secretAccessKey() );
+console.log( 'AwsAccountId :', sqs.awsAccountId() );
+
+sqs.getQueueUrl('my-queue', undefined, function(err, data) {
+    console.log("\nGetting the URL for my-queue - expecting success");
+    console.log('Error :', util.inspect(err, true, null));
+    console.log('Data :', util.inspect(data, true, null));
+});
