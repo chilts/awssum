f6cdff41d640f969e8f352081aa2d7d9f1e1cd59
Andrew Chilton <chilts@appsattic.com>
2011-10-25 16:16:43 +1300

    Add the unit tests for Amazon:S3, including checking strToSign() and signature()

---
+++ test/s3.js
@@ -0,0 +1,87 @@
+// --------------------------------------------------------------------------------------------------------------------
+//
+// s3.js - test for AWS Simple Notification Service
+//
+// Copyright (c) 2011 AppsAttic Ltd - http://www.appsattic.com/
+// Written by Andrew Chilton <chilts@appsattic.com>
+//
+// License: http://opensource.org/licenses/MIT
+//
+// --------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------
+// requires
+
+var tap = require("tap"),
+    test = tap.test,
+    plan = tap.plan,
+    _ = require('underscore');
+var amazon;
+var s3Service;
+
+// --------------------------------------------------------------------------------------------------------------------
+// basic tests
+
+test("load s3", function (t) {
+    s3Service = require("../lib/s3");
+    t.ok(s3Service, "object loaded");
+
+    amazon = require("../lib/amazon");
+    t.ok(amazon, "object loaded");
+
+    t.end();
+});
+
+test("create s3 object", function (t) {
+    var s3 = new s3Service.S3('access_key_id', 'secret_access_key', 'aws_account_id', amazon.US_WEST_1);
+
+    t.equal('access_key_id', s3.accessKeyId(), 'Access Key ID set properly');
+    t.equal('secret_access_key', s3.secretAccessKey(), 'Secret Access Key set properly');
+    t.equal('aws_account_id', s3.awsAccountId(), 'AWS Account ID set properly');
+    t.equal('California', s3.region(), 'Region is set properly');
+
+    t.end();
+});
+
+test("test all endpoints", function (t) {
+    var s31 = new s3Service.S3('access_key_id', 'secret_access_key', 'aws_account_id', amazon.US_EAST_1);
+    var s32 = new s3Service.S3('access_key_id', 'secret_access_key', 'aws_account_id', amazon.US_WEST_1);
+    var s33 = new s3Service.S3('access_key_id', 'secret_access_key', 'aws_account_id', amazon.EU_WEST_1);
+    var s34 = new s3Service.S3('access_key_id', 'secret_access_key', 'aws_account_id', amazon.AP_SOUTHEAST_1);
+    var s35 = new s3Service.S3('access_key_id', 'secret_access_key', 'aws_account_id', amazon.AP_NORTHEAST_1);
+
+    t.equal('s3.amazonaws.com', s31.endPoint(), '1) Endpoint is correct');
+    t.equal('s3.us-west-1.amazonaws.com', s32.endPoint(), '2) Endpoint is correct');
+    t.equal('s3.eu-west-1.amazonaws.com', s33.endPoint(), '3) Endpoint is correct');
+    t.equal('s3.ap-southeast-1.amazonaws.com', s34.endPoint(), '4) Endpoint is correct');
+    t.equal('s3.ap-northeast-1.amazonaws.com', s35.endPoint(), '5) Endpoint is correct');
+
+    t.end();
+});
+
+test("test strToSign", function (t) {
+    var s3 = new s3Service.S3('access_key_id', 'secret_access_key', 'aws_account_id', amazon.US_WEST_1);
+
+    var strToSignEmpty1 = s3.strToSign(undefined, undefined, 'GET', '/', {}, []);
+    t.equal(strToSignEmpty1, "GET\n\n\n\n/", 'strToSign of ListBuckets');
+
+    // doesn't matter _what_ these values are, we just need something (ie. 'version' doesn't matter if it's wrong)
+    var headers = {};
+    headers.Date = "Mon, 26 Oct 2011 16:07:36 Z";
+    var strToSign2 = s3.strToSign('bulk', undefined, 'POST', '/', headers, []);
+    t.equal(strToSign2, "POST\n\n\nMon, 26 Oct 2011 16:07:36 Z\n/bulk/", 'strToSign of common params');
+
+    t.end();
+});
+
+test("test signature", function (t) {
+    var s3 = new s3Service.S3('access_key_id', 'secret_access_key', 'aws_account_id', amazon.US_WEST_1);
+
+    var strToSign = "GET\n\n\nTue, 25 Oct 2011 03:09:21 UTC\n/";
+    var sigEmpty = s3.signature(strToSign);
+    t.equal(sigEmpty, 'OFs3nLlSvlN6EaeNy/IluZpS+E8=', 'signature of ListBuckets request');
+
+    t.end();
+});
+
+// --------------------------------------------------------------------------------------------------------------------
