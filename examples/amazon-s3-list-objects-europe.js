c687d733589e4df7999a7a693441b128b1240542
Andrew Chilton <chilts@appsattic.com>
2011-10-26 08:45:15 +1300

    Fix the Amazon S3 EndPoints!

---
+++ examples/amazon-s3-list-objects-europe.js
@@ -0,0 +1,27 @@
+var util = require('util');
+var amazon = require('../lib/amazon');
+var s3Service = require('../lib/s3');
+var _ = require('underscore');
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var s3 = new s3Service.S3(accessKeyId, secretAccessKey, awsAccountId, amazon.EU_WEST_1);
+
+console.log( 'Region :', s3.region() );
+console.log( 'EndPoint :',  s3.endPoint() );
+console.log( 'AccessKeyId :', s3.accessKeyId() );
+console.log( 'SecretAccessKey :', s3.secretAccessKey() );
+console.log( 'AwsAccountId :', s3.awsAccountId() );
+
+var options1 = {
+    BucketName : 'pie-18-in-europe',
+};
+
+s3.listObjects(options1, function(err, data) {
+    console.log("\nlisting objects in this bucket - expecting success");
+    console.log('Error :', util.inspect(err, true, null));
+    console.log('Data :', util.inspect(data, true, null));
+});
