98af23f2fe70f14981b67a3b582caebfb87b0130
Andrew Chilton <chilts@appsattic.com>
2011-10-27 16:56:51 +1300

    Add the S3:ObjectMetadata operation

---
+++ examples/amazon-s3-object-metadata.js
@@ -0,0 +1,38 @@
+var util = require('util');
+var amazon = require('../lib/amazon');
+var s3Service = require('../lib/s3');
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var s3 = new s3Service.S3(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', s3.region() );
+console.log( 'EndPoint :',  s3.endPoint() );
+console.log( 'AccessKeyId :', s3.accessKeyId() );
+console.log( 'SecretAccessKey :', s3.secretAccessKey() );
+console.log( 'AwsAccountId :', s3.awsAccountId() );
+
+var options1 = {
+    BucketName : 'pie-18',
+    ObjectName : 'test-object.txt',
+};
+
+s3.objectMetadata(options1, function(err, data) {
+    console.log("\ngetting an object metadata from /pie-18/test-object.txt - expecting success");
+    console.log('Error :', util.inspect(err, true, null));
+    console.log('Data :', util.inspect(data, true, null));
+});
+
+var options2 = {
+    BucketName : 'pie-18',
+    ObjectName : 'not-found.txt',
+};
+
+s3.objectMetadata(options2, function(err, data) {
+    console.log("\ngetting missing object - expecting failure");
+    console.log('Error :', util.inspect(err, true, null));
+    console.log('Data :', util.inspect(data, true, null));
+});
