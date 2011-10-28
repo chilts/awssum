1a9f86d3d188de3272d8ab0e4ddc13494a1e30c9
Andrew Chilton <chilts@appsattic.com>
2011-10-28 16:32:36 +1300

    Add the S3:GetObjectAcl and S3:GetObjectTorrent operations

---
+++ examples/amazon-s3-get-object-star.js
@@ -0,0 +1,33 @@
+var util = require('util');
+var amazon = require('../lib/amazon');
+var s3Service = require('../lib/s3');
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var s3 = new s3Service.S3(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', s3.region() );
+console.log( 'EndPoint :',  s3.endPoint() );
+console.log( 'AccessKeyId :', s3.accessKeyId() );
+console.log( 'SecretAccessKey :', s3.secretAccessKey() );
+console.log( 'AwsAccountId :', s3.awsAccountId() );
+
+var options = {
+    BucketName : 'pie-18',
+    ObjectName : 'test-object.txt',
+};
+
+s3.getObjectAcl(options, function(err, data) {
+    console.log("\ngetting an object acl - expecting success");
+    console.log('Error :', util.inspect(err, true, null));
+    console.log('Data :', util.inspect(data, true, null));
+});
+
+s3.getObjectTorrent(options, function(err, data) {
+    console.log("\ngetting an object torrent - expecting success");
+    console.log('Error :', util.inspect(err, true, null));
+    console.log('Data :', util.inspect(data, true, null));
+});
