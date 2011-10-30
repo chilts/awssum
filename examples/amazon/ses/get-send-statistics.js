afacae6c9ee230477d39040317100a6a235a40e1
Andrew Chilton <chilts@appsattic.com>
2011-10-30 23:31:23 +1300

    Add the SES ListVerifiedEmailAddresses, GetSendStatistics and GetSendQuote operations

---
+++ examples/amazon/ses/get-send-statistics.js
@@ -0,0 +1,22 @@
+var util = require('util');
+var amazon = require("amazon");
+var sesService = require("ses");
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var ses = new sesService.Ses(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', ses.region() );
+console.log( 'EndPoint :',  ses.endPoint() );
+console.log( 'AccessKeyId :', ses.accessKeyId() );
+// console.log( 'SecretAccessKey :', ses.secretAccessKey() );
+console.log( 'AwsAccountId :', ses.awsAccountId() );
+
+ses.getSendStatistics({}, function(err, data) {
+    console.log("\ngetting send statistics - expecting success");
+    console.log('Error :', util.inspect(err, true, null));
+    console.log('Data :', util.inspect(data, true, null));
+});
