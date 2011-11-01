75d55f03404571ae8f9dcac176202664e08529cf
Andrew Chilton <chilts@appsattic.com>
2011-11-01 21:32:21 +1300

    Add the Route53:{GetHostedZone,CreateHostedZone,DeleteHostedZone} operations

---
+++ examples/amazon/route53/get-hosted-zone.js
@@ -0,0 +1,22 @@
+var util = require('util');
+var amazon = require("amazon");
+var route53 = require("route53");
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var r53 = new route53.Route53(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', r53.region() );
+console.log( 'EndPoint :',  r53.endPoint() );
+console.log( 'AccessKeyId :', r53.accessKeyId() );
+// console.log( 'SecretAccessKey :', r53.secretAccessKey() );
+console.log( 'AwsAccountId :', r53.awsAccountId() );
+
+r53.getHostedZone({ hostedZoneId : 'deadbeef' }, function(err, data) {
+    console.log("\ngetting hosted zone - expecting failure");
+    console.log('Error :', util.inspect(err, true, null));
+    console.log('Data :', util.inspect(data, true, null));
+});
