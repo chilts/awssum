f6da1a835621075abda2b4e52a7dd735b867f143
Andrew Chilton <chilts@appsattic.com>
2011-11-01 13:49:26 +1300

    Add the ELB:DescribeLoadBalancers operation

---
+++ examples/amazon/elb/describe-load-balancers.js
@@ -0,0 +1,22 @@
+var util = require('util');
+var amazon = require("amazon");
+var elbService = require("elb");
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var elb = new elbService.Elb(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', elb.region() );
+console.log( 'EndPoint :',  elb.endPoint() );
+console.log( 'AccessKeyId :', elb.accessKeyId() );
+// console.log( 'SecretAccessKey :', elb.secretAccessKey() );
+console.log( 'AwsAccountId :', elb.awsAccountId() );
+
+elb.describeLoadBalancers({}, function(err, data) {
+    console.log("\ndescribing load balancers - expecting success");
+    console.log('Error :', util.inspect(err, true, null));
+    console.log('Data :', util.inspect(data, true, null));
+});
