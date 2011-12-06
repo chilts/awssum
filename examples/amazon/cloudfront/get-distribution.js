25b9203aaa25a3cd02cf123e2448e5fbcf607801
Andrew Chilton <chilts@appsattic.com>
2011-12-06 16:24:21 +1300

    Add the CloudFront:GetDistribution operation

---
+++ examples/amazon/cloudfront/get-distribution.js
@@ -0,0 +1,26 @@
+var util = require('util');
+var amazon = require("amazon/amazon");
+var cloudFrontService = require("amazon/cloudfront");
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var cloudFront = new cloudFrontService.CloudFront(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', cloudFront.region() );
+console.log( 'EndPoint :',  cloudFront.host() );
+console.log( 'AccessKeyId :', cloudFront.accessKeyId() );
+// console.log( 'SecretAccessKey :', cloudFront.secretAccessKey() );
+console.log( 'AwsAccountId :', cloudFront.awsAccountId() );
+
+var args = {
+    DistributionId : 'HelloWorld',
+};
+
+cloudFront.GetDistribution(args, function(err, data) {
+    console.log("\nget distribution - expecting failure (doesn't exist)");
+    console.log('Error :', util.inspect(err, true, null));
+    console.log('Data :', util.inspect(data, true, null));
+});
