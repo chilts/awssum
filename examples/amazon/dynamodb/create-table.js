94a4f0c1abade040034a78607be6293ea5eedf73
Andrew Chilton <chilts@appsattic.com>
2012-02-07 19:20:58 +1300

    Add the Amazon:DynamoDB:CreateTable operation

---
+++ examples/amazon/dynamodb/create-table.js
@@ -0,0 +1,31 @@
+var inspect = require('eyes').inspector();
+var awssum = require('awssum');
+var amazon = awssum.load('amazon/amazon');
+var dynamoDBService = awssum.load('amazon/dynamodb');
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var ddb = new dynamoDBService(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', ddb.region() );
+console.log( 'EndPoint :',  ddb.host() );
+console.log( 'AccessKeyId :', ddb.accessKeyId() );
+// console.log( 'SecretAccessKey :', ddb.secretAccessKey() );
+console.log( 'AwsAccountId :', ddb.awsAccountId() );
+
+var data = {
+    TableName : 'test',
+    HashAttrName : 'id',
+    HashAttrType : 'S',
+    ReadCapacityUnits : 5,
+    WriteCapacityUnits : 5,
+};
+
+ddb.CreateTable(data, function(err, data) {
+    console.log("\ncreating a table - expecting success");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
