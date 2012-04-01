ead5448ca53e7779707b7a8bedade014774c6586
Andrew Chilton <chilts@appsattic.com>
2012-04-01 20:47:08 +1200

    Add the Amazon:DynamoDB:GetItem operation

---
+++ examples/amazon/dynamodb/get-item.js
@@ -0,0 +1,34 @@
+var inspect = require('eyes').inspector();
+var awssum = require('awssum');
+var amazon = awssum.load('amazon/amazon');
+var dynamoDBService = awssum.load('amazon/dynamodb');
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var ddb = new dynamoDBService(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', ddb.region() );
+console.log( 'EndPoint :',  ddb.host() );
+console.log( 'AccessKeyId :', ddb.accessKeyId() );
+// console.log( 'SecretAccessKey :', ddb.secretAccessKey() );
+console.log( 'AwsAccountId :', ddb.awsAccountId() );
+
+var user1 = {
+    TableName : 'test',
+    Key : {
+        HashKeyElement : { S : '9bcd1573-00a5-4676-9f9c-9581c8060777' },
+    },
+    AttributesToGet : [
+        'id', 'username', 'password', 'logins', 'inserted', 'updated'
+    ],
+    ConsistentRead : true,
+};
+
+ddb.GetItem(user1, function(err, data) {
+    console.log("\ngetting item1 - expecting success");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
