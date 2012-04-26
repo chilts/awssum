6b388dc34ec62bcd677a1c7bcd61f4dfee935673
Andrew Chilton <chilts@appsattic.com>
2012-04-26 14:50:58 +1200

    Fix up the term.ie OAuth test server

--- examples/term/request-token.js
+++ examples/term/request-token.js
@@ -6,14 +6,21 @@ var termService = awssum.load('term/term');
 var env = process.env;
 var consumerKey = process.env.TERM_CONSUMER_KEY;
 var consumerSecret = process.env.TERM_CONSUMER_SECRET;
+// don't need the token, tokenSecret or verifier
 
-var term = new termService(consumerKey, consumerSecret);
+var term = new termService.Term(consumerKey, consumerSecret);
 
-console.log( 'OAuthConsumerKey :', term.oauthConsumerKey() );
-console.log( 'OAuthConsumerSecret :',  term.oauthConsumerSecret() );
+console.log( 'ConsumerKey :', term.consumerKey() );
+console.log( 'ConsumerSecret :',  term.consumerSecret() );
 
-term.RequestToken({ 'oauth_callback' : 'oob' }, function(err, data) {
+term.RequestToken({ 'OAuthCallback' : 'oob' }, function(err, data) {
     console.log("\nrequesting token - expecting success");
     inspect(err, 'Error');
     inspect(data, 'Data');
+
+    console.log( 'If you want to verify this token, visit: '
+                 + term.protocol() + '://' + term.authorizeHost()
+                 + term.authorizePath()
+                 + '?oauth_token=' + data.Body.oauth_token
+               );
 });
