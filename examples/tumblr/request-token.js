3c6cfddbcd25ad54b1cdc182b1bc0047c8a79a05
Andrew Chilton <chilts@appsattic.com>
2012-04-26 14:54:15 +1200

    Fix Tumblr a bit (though not complete)

--- examples/tumblr/request-token.js
+++ examples/tumblr/request-token.js
@@ -6,14 +6,24 @@ var tumblrService = awssum.load('tumblr/tumblr');
 var env = process.env;
 var consumerKey = process.env.TUMBLR_CONSUMER_KEY;
 var consumerSecret = process.env.TUMBLR_CONSUMER_SECRET;
+// don't need the token, tokenSecret or verifier
 
 var tumblr = new tumblrService.Tumblr(consumerKey, consumerSecret);
 
-console.log( 'OAuthConsumerKey :', tumblr.oauthConsumerKey() );
-console.log( 'OAuthConsumerSecret :',  tumblr.oauthConsumerSecret() );
+console.log( 'ConsumerKey :', tumblr.consumerKey() );
+console.log( 'ConsumerSecret :',  tumblr.consumerSecret() );
 
-tumblr.RequestToken({ 'oauth_callback' : 'oob' }, function(err, data) {
+tumblr.RequestToken({ 'OAuthCallback' : 'oob' }, function(err, data) {
     console.log("\nrequesting token - expecting success");
     inspect(err, 'Error');
+    if ( err ) {
+        process.exit();
+    }
+
     inspect(data, 'Data');
+    console.log( 'If you want to verify this token, visit: '
+                 + tumblr.protocol() + '://' + tumblr.authorizeHost()
+                 + tumblr.authorizePath()
+                 + '?oauth_token=' + data.Body.oauth_token
+               );
 });
