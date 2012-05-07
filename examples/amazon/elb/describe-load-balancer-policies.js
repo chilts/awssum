3f0880ef5f6d148a57ffc7f5ee1f165e018c1527
Andrew Chilton <chilts@appsattic.com>
2012-05-08 10:11:03 +1200

    Add these two Amazon:ELB examples

---
+++ examples/amazon/elb/describe-load-balancer-policies.js
@@ -0,0 +1,22 @@
+var inspect = require('eyes').inspector();
+var amazon = require("amazon/amazon");
+var elbService = require("amazon/elb");
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var elb = new elbService(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', elb.region() );
+console.log( 'EndPoint :',  elb.host() );
+console.log( 'AccessKeyId :', elb.accessKeyId() );
+// console.log( 'SecretAccessKey :', elb.secretAccessKey() );
+console.log( 'AwsAccountId :', elb.awsAccountId() );
+
+elb.DescribeLoadBalancerPolicies(function(err, data) {
+    console.log("\ndescribing load balancer policies - expecting success");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
---
+++ examples/amazon/elb/describe-load-balancer-policy-types.js
@@ -0,0 +1,23 @@
+var inspect = require('eyes').inspector();
+var awssum = require('awssum');
+var amazon = awssum.load('amazon/amazon');
+var elbService = awssum.load('amazon/elb');
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var elb = new elbService(accessKeyId, secretAccessKey, awsAccountId, amazon.US_EAST_1);
+
+console.log( 'Region :', elb.region() );
+console.log( 'EndPoint :',  elb.host() );
+console.log( 'AccessKeyId :', elb.accessKeyId() );
+// console.log( 'SecretAccessKey :', elb.secretAccessKey() );
+console.log( 'AwsAccountId :', elb.awsAccountId() );
+
+elb.DescribeLoadBalancerPolicyTypes(function(err, data) {
+    console.log("\ndescribing load balancer policy types - expecting success");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
