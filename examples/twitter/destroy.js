d13ba7a8de9b728ae85a69f61acc95a70d74c81a
Andrew Chilton <chilts@appsattic.com>
2012-05-09 10:13:16 +1200

    Add a destroy example for Twitter:Twitter

---
+++ examples/twitter/destroy.js
@@ -0,0 +1,31 @@
+var inspect = require('eyes').inspector({ maxLength : 65536 });
+var commander = require('commander');
+var awssum = require('awssum');
+var oauth = awssum.load('oauth');
+var twitterService = awssum.load('twitter/twitter');
+
+var env = process.env;
+var consumerKey = process.env.TWITTER_CONSUMER_KEY;
+var consumerSecret = process.env.TWITTER_CONSUMER_SECRET;
+var token = process.env.TWITTER_TOKEN;
+var tokenSecret = process.env.TWITTER_TOKEN_SECRET;
+// don't need the verifier
+
+var twitter = new twitterService.Twitter(consumerKey, consumerSecret);
+twitter.setToken(token);
+twitter.setTokenSecret(tokenSecret);
+
+console.log( 'ConsumerKey    :', twitter.consumerKey()                          );
+console.log( 'ConsumerSecret :', twitter.consumerSecret().substr(0, 3) + '...'  );
+console.log( 'Token          :', twitter.token()                                );
+console.log( 'TokenSecret    :', twitter.tokenSecret().substr(0, 3) + '...'     );
+
+var data = {
+    id : '199984909111136257',
+};
+
+twitter.Destroy(data, function(err, data) {
+    console.log('\ncalling statuses/update - expecting success');
+    inspect(err, 'Err');
+    inspect(data, 'Data');
+});
