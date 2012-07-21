88ff9c76e03409a4f86b6756fee2919d26484720
Andrew Chilton <chilts@appsattic.com>
2012-07-21 20:02:59 +1200

    Make sure we scroll to the top once a hash is set

--- s/js/ready.js
+++ s/js/ready.js
@@ -3,8 +3,9 @@ $(function() {
     // Bind the event.
     $(window).hashchange(function(){
         $('.nav li').removeClass('active');
-        $('#' + location.hash).addClass('active');
-    })
+        $(location.hash).addClass('active');
+        $(window).scrollTop();
+    });
 
     // trigger the hashchange event on pageload
     $(window).hashchange();
