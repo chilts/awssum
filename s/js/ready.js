a8cbf444e89c9871c1763d789e75755792ab6cc6
Andrew Chilton <chilts@appsattic.com>
2012-07-21 20:17:37 +1200

    Fix the method name on window to scrollTo()

--- s/js/ready.js
+++ s/js/ready.js
@@ -4,7 +4,7 @@ $(function() {
     $(window).hashchange(function(){
         $('.nav li').removeClass('active');
         $('.' + location.hash.substr(1)).addClass('active');
-        window.scrollTop(0, 0);
+        window.scrollTo(0, 0);
     });
 
     // trigger the hashchange event on pageload
