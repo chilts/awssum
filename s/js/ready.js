0b1053635e8dbd2bf97ff6f63ac7a8e04f8a60b7
Andrew Chilton <chilts@appsattic.com>
2012-07-21 20:20:52 +1200

    Make all the tab classnames start with tab-*

--- s/js/ready.js
+++ s/js/ready.js
@@ -3,11 +3,13 @@ $(function() {
     // Bind the event.
     $(window).hashchange(function(){
         $('.nav li').removeClass('active');
-        $('.' + location.hash.substr(1)).addClass('active');
+        $('.tab-' + location.hash.substr(1)).addClass('active');
         window.scrollTo(0, 0);
     });
 
     // trigger the hashchange event on pageload
-    $(window).hashchange();
+    if ( location.hash ) {
+        $(window).hashchange();
+    }
 
 });
