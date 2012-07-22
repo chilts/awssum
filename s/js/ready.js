6ba0804bbac960820dd9eb843866bf90fc93aaef
Andrew Chilton <chilts@appsattic.com>
2012-07-22 16:04:32 +1200

    Add the proper Twitter follow button

--- s/js/ready.js
+++ s/js/ready.js
@@ -85,4 +85,12 @@ $(function() {
         }
     }, 5000);
 
+    // Twitter "Follow" button (from https://twitter.com/about/resources/buttons#follow)
+    var js, fjs = documents.getElementsByTagName('script')[0];
+    if ( !document.getElementById('twitter-wjs') ) {
+        js = document.createElement('script');
+        js.id = 'twitter-wjs';
+        js.src = '//platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }
 });
