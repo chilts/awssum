0f95a3227f8dc3ca9b6004fd057b47c5e9a09a95
Andrew Chilton <chilts@appsattic.com>
2012-07-25 22:10:54 +1200

    Add all 70ish operations for Amazon:IAM service

---
+++ examples/amazon/iam/put-group-policy.js
@@ -0,0 +1,36 @@
+var inspect = require('eyes').inspector();
+var awssum = require('awssum');
+var amazon = awssum.load('amazon/amazon');
+var Iam = awssum.load('amazon/iam').Iam;
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var iam = new Iam({
+    'accessKeyId'     : accessKeyId,
+    'secretAccessKey' : secretAccessKey,
+    // 'awsAccountId'    : awsAccountId, // optional
+    'region'          : amazon.US_EAST_1
+});
+
+console.log( 'Region :', iam.region() );
+console.log( 'EndPoint :',  iam.host() );
+console.log( 'AccessKeyId :', iam.accessKeyId().substr(0,3) + '...' );
+console.log( 'SecretAccessKey :', iam.secretAccessKey().substr(0,3) + '...' );
+console.log( 'AwsAccountId :', iam.awsAccountId() );
+
+var opts = {
+    GroupName : 'test-group',
+    PolicyDocument : {
+        'msg' : 'Hello, World!'
+    },
+    PolicyName : 'text-policy'
+};
+
+iam.PutGroupPolicy(opts, function(err, data) {
+    console.log("\nputting group policy - expecting failure (group not found)");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
