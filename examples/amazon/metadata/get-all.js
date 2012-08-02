b3d806b5706a463f3c434af9723c66f03d258fb2
Andrew Chilton <chilts@appsattic.com>
2012-08-02 18:47:44 +1200

    Add support for all Amazon:MetaData operations

---
+++ examples/amazon/metadata/get-all.js
@@ -0,0 +1,34 @@
+var inspect = require('eyes').inspector();
+var awssum = require('awssum');
+var MetaData = awssum.load('amazon/metadata').MetaData;
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var md = new MetaData();
+
+md.Get(function(err, data) {
+    console.log("\ngetting metadata - expecting failure (no Category given)");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
+
+md.Get({}, function(err, data) {
+    console.log("\ngetting metadata - expecting failure (no Category given)");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
+
+md.Get({ Version : 'latest', Category : '/' }, function(err, data) {
+    console.log("\ngetting metadata - expecting success");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
+
+md.Get({ Version : 'latest', Category : '/latest/meta-data/' }, function(err, data) {
+    console.log("\ngetting metadata - expecting success");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
