2358118fec2447680dacccf0e938370192e279d3
Andrew Chilton <chilts@appsattic.com>
2012-08-04 21:51:17 +1200

    Add all of the operations for Amazon:ImportExport

---
+++ examples/amazon/importexport/list-jobs.js
@@ -0,0 +1,26 @@
+var inspect = require('eyes').inspector();
+var awssum = require('awssum');
+var amazon = awssum.load('amazon/amazon');
+var ImportExport = awssum.load('amazon/importexport').ImportExport;
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var ie = new ImportExport({
+    'accessKeyId'     : accessKeyId,
+    'secretAccessKey' : secretAccessKey,
+    'region'          : amazon.US_EAST_1
+});
+
+console.log( 'Region :', ie.region() );
+console.log( 'EndPoint :',  ie.host() );
+console.log( 'AccessKeyId :', ie.accessKeyId().substr(0,3) + '...' );
+console.log( 'SecretAccessKey :', ie.secretAccessKey().substr(0,3) + '...' );
+
+ie.ListJobs(function(err, data) {
+    console.log("\nlisting jobs - expecting success");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
