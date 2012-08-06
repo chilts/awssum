d362a17e6f2582bc2648a64a0029c4b7877f900c
Andrew Chilton <chilts@appsattic.com>
2012-08-06 17:46:58 +1200

    Make sure we have the example checked in

---
+++ examples/amazon/storagegateway/list-gateways.js
@@ -0,0 +1,34 @@
+var inspect = require('eyes').inspector();
+var awssum = require('awssum');
+var amazon = awssum.load('amazon/amazon');
+var StorageGateway = awssum.load('amazon/storagegateway').StorageGateway;
+
+var env = process.env;
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var awsAccountId = process.env.AWS_ACCOUNT_ID;
+
+var sg = new StorageGateway({
+    'accessKeyId'     : accessKeyId,
+    'secretAccessKey' : secretAccessKey,
+    // 'awsAccountId'    : awsAccountId, // optional
+    'region'          : amazon.US_EAST_1
+});
+
+console.log( 'Region :', sg.region() );
+console.log( 'EndPoint :',  sg.host() );
+console.log( 'AccessKeyId :', sg.accessKeyId().substr(0,3) + '...' );
+console.log( 'SecretAccessKey :', sg.secretAccessKey().substr(0,3) + '...' );
+console.log( 'AwsAccountId :', sg.awsAccountId() );
+
+sg.ListGateways(function(err, data) {
+    console.log("\nlisting gateways - expecting success");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
+
+sg.ListGateways({ Limit : 5 }, function(err, data) {
+    console.log("\nlisting gateways with a limit of 5 - expecting success");
+    inspect(err, 'Error');
+    inspect(data, 'Data');
+});
