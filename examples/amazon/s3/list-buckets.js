912a74ae658bb0f060a5a7c1ae2756e9a3a3792f
Andrew Chilton <chilts@appsattic.com>
2012-08-19 18:30:13 +1200

    New version of the example Amazon:S3:ListBuckets

--- examples/amazon/s3/list-buckets.js
+++ examples/amazon/s3/list-buckets.js
@@ -1,45 +1,54 @@
-var inspect = require('eyes').inspector();
+var fmt = require('fmt');
 var awssum = require('awssum');
 var amazon = awssum.load('amazon/amazon');
 var S3 = awssum.load('amazon/s3').S3;
 
-var env = process.env;
-var accessKeyId = process.env.ACCESS_KEY_ID;
-var secretAccessKey = process.env.SECRET_ACCESS_KEY;
-var awsAccountId = process.env.AWS_ACCOUNT_ID;
+var env             = process.env;
+var accessKeyId     = env.ACCESS_KEY_ID;
+var secretAccessKey = env.SECRET_ACCESS_KEY;
+var awsAccountId    = env.AWS_ACCOUNT_ID;
 
 var s3 = new S3({
-    'accessKeyId' : accessKeyId,
+    'accessKeyId'     : accessKeyId,
     'secretAccessKey' : secretAccessKey,
-    'region' : amazon.US_EAST_1
+    'region'          : amazon.US_EAST_1,
 });
 
-console.log( 'Region :', s3.region() );
-console.log( 'EndPoint :',  s3.host() );
-console.log( 'AccessKeyId :', s3.accessKeyId() );
-console.log( 'SecretAccessKey :', s3.secretAccessKey().substr(0, 3) + '...' );
-console.log( 'AwsAccountId :', s3.awsAccountId() );
+fmt.line();
+fmt.title('s3.ListBuckets');
+
+fmt.field('Region',          s3.region());
+fmt.field('EndPoint',        s3.host() );
+fmt.field('AccessKeyId',     s3.accessKeyId() );
+fmt.field('SecretAccessKey', s3.secretAccessKey().substr(0, 3) + '...' );
+fmt.field('AwsAccountId',    s3.awsAccountId() );
+
+fmt.line();
 
 s3.ListBuckets(function(err, data) {
-    console.log("\nlisting all the buckets (no options given) - expecting success");
-    inspect(err, 'Error');
-    inspect(data, 'Data');
+    fmt.msg("listing all the buckets (no options given) - expecting success");
+    fmt.dump(err,  'Error');
+    fmt.dump(data, 'Data');
+    fmt.line();
 });
 
 s3.ListBuckets(undefined, function(err, data) {
-    console.log("\nlisting all the buckets (undefined options) - expecting success");
-    inspect(err, 'Error');
-    inspect(data, 'Data');
+    fmt.msg("listing all the buckets (undefined options) - expecting success");
+    fmt.dump(err,  'Error');
+    fmt.dump(data, 'Data');
+    fmt.line();
 });
 
 s3.ListBuckets({}, function(err, data) {
-    console.log("\nlisting all the buckets (empty options) - expecting success");
-    inspect(err, 'Error');
-    inspect(data, 'Data');
+    fmt.msg("listing all the buckets (empty options) - expecting success");
+    fmt.dump(err,  'Error');
+    fmt.dump(data, 'Data');
+    fmt.line();
 });
 
 s3.ListBuckets({ Ignored : 'this is' }, function(err, data) {
-    console.log("\nlisting all the buckets (nothing interesting in options) - expecting success");
-    inspect(err, 'Error');
-    inspect(data, 'Data');
+    fmt.msg("listing all the buckets (nothing interesting in options) - expecting success");
+    fmt.dump(err,  'Error');
+    fmt.dump(data, 'Data');
+    fmt.line();
 });
