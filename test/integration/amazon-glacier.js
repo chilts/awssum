f2275a568ef782d9f19f4698afb90e5571ca1662
Andrew Chilton <chilts@appsattic.com>
2012-08-21 22:13:50 +1200

    Add the Amazon:Glacier integration test

---
+++ test/integration/amazon-glacier.js
@@ -0,0 +1,50 @@
+// --------------------------------------------------------------------------------------------------------------------
+//
+// integration/amazon-glacier.js - integration tests for Amazon Storage Gateway
+//
+// Copyright (c) 2012 AppsAttic Ltd - http://www.appsattic.com/
+// Written by Andrew Chilton <chilts@appsattic.com>
+//
+// License: http://opensource.org/licenses/MIT
+//
+// --------------------------------------------------------------------------------------------------------------------
+// requires
+
+var fs = require('fs');
+var test = require('tap').test;
+var awssum = require('../../');
+var amazon = awssum.load('amazon/amazon');
+var glacier = awssum.load('amazon/glacier').Glacier;
+var inspect = require('eyes').inspector();
+
+// --------------------------------------------------------------------------------------------------------------------
+
+var env = process.env;
+var glacier;
+try {
+    glacier = new Glacier({
+        'accessKeyId'     : env.ACCESS_KEY_ID,
+        'secretAccessKey' : env.SECRET_ACCESS_KEY,
+        'awsAccountId'    : env.AWS_ACCOUNT_ID,
+        'region'          : amazon.US_EAST_1
+    });
+}
+catch(e) {
+    // env vars aren't set, so skip these integration tests
+    process.exit();
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// Amazon:Glacier operations
+
+// just check one request, checks the signature to be honest
+test('Glacier:ListVaults - (1) Standard', function(t) {
+    var opts = {};
+    glacier.ListVaults(function(err, data) {
+        t.notOk(err, 'Glacier:ListVaults - standard : no error');
+        t.ok(data, 'Glacier:ListVaults - standard : result ok');
+        t.end();
+    });
+});
+
+// --------------------------------------------------------------------------------------------------------------------
