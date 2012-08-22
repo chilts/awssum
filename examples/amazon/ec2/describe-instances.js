5a84e3b79cc914fb657e27a7ad63c7c0fff051ba
Andrew Chilton <chilts@appsattic.com>
2012-08-22 22:02:46 +1200

    Fixes #56 : Fix the Amazon:EC2:DescribeInstances examples with Filters

--- examples/amazon/ec2/describe-instances.js
+++ examples/amazon/ec2/describe-instances.js
@@ -15,23 +15,40 @@ var ec2 = new Ec2({
     'region'          : amazon.US_EAST_1
 });
 
+fmt.line();
+fmt.title('ec2.DescribeInstances');
+
 fmt.field('Region', ec2.region() );
 fmt.field('EndPoint', ec2.host() );
 fmt.field('AccessKeyId', ec2.accessKeyId().substr(0,3) + '...' );
 fmt.field('SecretAccessKey', ec2.secretAccessKey().substr(0,3) + '...' );
 fmt.field('AwsAccountId', ec2.awsAccountId() );
 
+fmt.line();
+
 ec2.DescribeInstances(function(err, data) {
     fmt.msg("Describing instances - expecting success");
     fmt.dump(err, 'Error');
     fmt.dump(data, 'Data');
+    fmt.line();
 });
 
-ec2.DescribeInstances({
-    FilterName  : [ 'ip-address', 'key-name' ],
-    FilterValue : [ [ '1.2.3.4', '5.6.7.8' ], [ 'my-key' ] ],
-}, function(err, data) {
+var args = {
+    Filter : [
+        {
+            Name : 'availability-zone',
+            Value : [ 'us-east-1', 'us-west-1' ],
+        },
+        {
+            Name : 'instance-id',
+            Value : [ 'i-7a00642e' ],
+        },
+    ],
+};
+
+ec2.DescribeInstances(args, function(err, data) {
     fmt.msg("Describing instances (with filter) - expecting success");
     fmt.dump(err, 'Error');
     fmt.dump(data, 'Data');
+    fmt.line();
 });
