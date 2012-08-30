b4bb0bfb7a58205b07fa13494edc2555b5e27fa9
Andrew Chilton <chilts@appsattic.com>
2012-08-30 18:02:30 +1200

    Fixes #70 : Allow all the other params to be given to Amazon::CloudSearch::Search

--- examples/amazon/cloudsearch/search.js
+++ examples/amazon/cloudsearch/search.js
@@ -17,3 +17,26 @@ ss.Search(opts, function(err, data) {
     fmt.dump(err, 'Error');
     fmt.dump(data, 'Data');
 });
+
+var argsWithFields = {
+    'q'      : 'searchterm',
+    'facet'  : 'size,color,section', // comma separated fields
+    'field' : {
+        'color' : {
+            'constraints' : "'red','green','blue'",
+            'sort'        : 'alpha',
+            'top-n'       : 10,
+        },
+        'year' : {
+            'constraints' : '2000..2011',
+            'sort'        : 'count',
+            'top-n'       : 50,
+        },
+    },
+};
+
+ss.Search(argsWithFields, function(err, data) {
+    fmt.msg("searching for something - expecting failure (ENOTFOUND ie. no search Domain(Id/Name))");
+    fmt.dump(err, 'Error');
+    fmt.dump(data, 'Data');
+});
